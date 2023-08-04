039d154e1e30488beeddbd70bbdd064a56dd563d
mitsufrushio <mitsu.junior1215@outlook.com>
2023-08-04 01:13:32 -0300

    03/08

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './auth/auth.guard';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent,canActivate: [authGuard], children: [
       {path: 'home', component: HomeComponent },
-      {path: 'clientes', component: ClienteListComponent}
+      {path: 'clientes', component: ClienteListComponent},
+      {path: 'clientes/create', component: ClienteCreateComponent}
     ]
   }
 ];
--- src/app/components/cliente/cliente-create/cliente-create.component.ts
+++ src/app/components/cliente/cliente-create/cliente-create.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-create',
   templateUrl: './cliente-create.component.html',
   styleUrls: ['./cliente-create.component.css']
 })
-export class ClienteCreateComponent {
+export class ClienteCreateComponent implements OnInit {
+  
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  idade: FormControl = new FormControl(null, Validators.required);
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  fone: FormControl = new FormControl(null, Validators.minLength(3));
+  constructor () {
 
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+  validaCampos(): boolean {
+    return  this.nome.valid  && 
+            this.cpf.valid   && 
+            this.email.valid && 
+            this.idade.valid &&
+            this.fone.valid;
+  }
 }
